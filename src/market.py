82e283e9581738172a4adabf61051a99c03437b9
unknown <walter@LDE-L08.logodata.intern>
2021-04-10 22:26:44 +0200

    use esi connector

--- src/market.py
+++ src/market.py
@@ -1,118 +1,12 @@
 import http.client
-from terminaltables import AsciiTable
 import json
+from src.esi import esi
+from src import helper
+from conf import regions
+from terminaltables import AsciiTable
 import locale 
 locale.setlocale(locale.LC_ALL, 'de_DE')
 
-
-regions = [
-  10000001,
-  10000002,
-  10000003,
-  10000004,
-  10000005,
-  10000006,
-  10000007,
-  10000008,
-  10000009,
-  10000010,
-  10000011,
-  10000012,
-  10000013,
-  10000014,
-  10000015,
-  10000016,
-  10000017,
-  10000018,
-  10000019,
-  10000020,
-  10000021,
-  10000022,
-  10000023,
-  10000025,
-  10000027,
-  10000028,
-  10000029,
-  10000030,
-  10000031,
-  10000032,
-  10000033,
-  10000034,
-  10000035,
-  10000036,
-  10000037,
-  10000038,
-  10000039,
-  10000040,
-  10000041,
-  10000042,
-  10000043,
-  10000044,
-  10000045,
-  10000046,
-  10000047,
-  10000048,
-  10000049,
-  10000050,
-  10000051,
-  10000052,
-  10000053,
-  10000054,
-  10000055,
-  10000056,
-  10000057,
-  10000058,
-  10000059,
-  10000060,
-  10000061,
-  10000062,
-  10000063,
-  10000064,
-  10000065,
-  10000066,
-  10000067,
-  10000068,
-  10000069,
-  11000001,
-  11000002,
-  11000003,
-  11000004,
-  11000005,
-  11000006,
-  11000007,
-  11000008,
-  11000009,
-  11000010,
-  11000011,
-  11000012,
-  11000013,
-  11000014,
-  11000015,
-  11000016,
-  11000017,
-  11000018,
-  11000019,
-  11000020,
-  11000021,
-  11000022,
-  11000023,
-  11000024,
-  11000025,
-  11000026,
-  11000027,
-  11000028,
-  11000029,
-  11000030,
-  11000031,
-  11000032,
-  11000033,
-  12000001,
-  12000002,
-  12000003,
-  12000004,
-  12000005,
-  13000001
-]
 
 conf = { }
 conn = object()
@@ -287,31 +181,41 @@ def find_deal(order_type,type_name):
         price = find_max(get_pages(conn,conf.region,type_id,order_type))
     else:
         price = find_min(get_pages(conn,conf.region,type_id,order_type))
-    
-
     return price
-    
 
-def run(itemlist):
-    global conf
-    global conn
-    
-    data = '[ "'+'", "'.join(itemlist)+ '" ]'
-    conn = http.client.HTTPSConnection(conf.server)
-    conn.request('POST',conf.routes['ids']+'/?datasource=tranquility&language=de',data,conf.headers)
 
-    response = conn.getresponse()
-    result = response.read().decode()
-    types = json.loads(result)
-
-    m = [ ['Name', 'Preis'], ]
-    for item in itemlist:
-        price = find_max(get_pages(conn,conf.region,get_id(item,types['inventory_types']),'buy'))
-        total = locale.format_string('%.2f', price, True, True)
-        m.append([item,total])
+def scan(itemlist: helper.items, region=''):
+    eve = esi()
+    eve.connect()
+    regionlist = helper.items(regions.regions)
+    if region == '':
+        region = 'The Forge'
+    regionid = regionlist.getByName(region)
+    m = [ ['Name', 'Kauf', 'Verkauf'], ]
+    for item in itemlist.items:
+        buy = helper.find_max(get_orders(eve, regionid, item['id'], 'buy'))
+        total_b = locale.format_string('%.2f', buy, True, True)
+        sell = helper.find_min(get_orders(eve, regionid, item['id'], 'sell'))
+        total_s = locale.format_string('%.2f', sell, True, True)
+        m.append([item['name'].replace('Compressed', 'comp.'), total_b, total_s])
     t = AsciiTable(m)
     t.justify_columns[1] = 'right'
+    t.justify_columns[2] = 'right'
     t.inner_heading_row_border = True
     t.inner_row_border = False
     out_string = '```' + t.table + '```'
     return out_string
+
+def get_orders(eve, region_id, item_id, order_type):
+    page = 1
+    json_string = ""
+    while 1==1:
+        orders = eve.request('market', {'region': region_id, 'order_type': order_type, 'page': page, 'type_id': item_id }, '')
+        if orders == '{"error":"Requested page does not exist!"}':
+            break
+        if json_string == "":
+            json_string += orders
+        else:
+            json_string += ', '+orders
+        page += 1
+    return json.loads('['+json_string.replace('[','').replace(']','')+']')
