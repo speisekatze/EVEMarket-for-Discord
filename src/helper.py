46461d2a0a7d42b35a1a82cf1ca5ae1fe9413b2c
unknown <walter@LDE-L08.logodata.intern>
2021-04-10 21:57:26 +0200

    move min/max to helper

--- src/helper.py
+++ src/helper.py
@@ -25,3 +25,29 @@ class items:
     
     def getNames(self):
         return self.ids.values()
+
+def find_max(market):
+    global order
+    max_price = 0
+    for m in market:
+        if max_price == 0:
+            max_price = float(m['price'])
+            order = m
+            continue
+        if float(m['price']) > max_price:
+            max_price = float(m['price'])
+            order = m
+    return max_price
+
+def find_min(market):
+    global order
+    min_price = 0
+    for m in market:
+        if min_price == 0:
+            min_price = float(m['price'])
+            order = m
+            continue
+        if float(m['price']) < min_price:
+            min_price = float(m['price'])
+            order = m
+    return min_price
