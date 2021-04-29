adb3534071a7a51934c9b6b75a54e4e25a6f9fed
Thomas Walter <speisekatze@gmail.com>
2021-04-30 01:31:24 +0200

    more itemlists

--- conf/itemnames.py
+++ conf/itemnames.py
@@ -39,3 +39,64 @@ minerals = [
     { "id": 11399, "name": "Morphite" },
     { "id": 48927, "name": "Chromodynamic Tricarboxyls" }
 ]
+
+moon = [
+  { "id": 16633, "name": "Hydrocarbons" },
+  { "id": 16634, "name": "Atmospheric Gases" },
+  { "id": 16635, "name": "Evaporite Deposits" },
+  { "id": 16636, "name": "Silicates" },
+  { "id": 16637, "name": "Tungsten" },
+  { "id": 16638, "name": "Titanium" },
+  { "id": 16639, "name": "Scandium" },
+  { "id": 16640, "name": "Cobalt" },
+  { "id": 16641, "name": "Chromium" },
+  { "id": 16642, "name": "Vanadium" },
+  { "id": 16643, "name": "Cadmium" },
+  { "id": 16644, "name": "Platinum" },
+  { "id": 16646, "name": "Mercury" },
+  { "id": 16647, "name": "Caesium" },
+  { "id": 16648, "name": "Hafnium" },
+  { "id": 16649, "name": "Technetium" },
+  { "id": 16650, "name": "Dysprosium" },
+  { "id": 16651, "name": "Neodymium" },
+  { "id": 16652, "name": "Promethium" },
+  { "id": 16653, "name": "Thulium" }
+]
+
+gas = [
+  {  "id": 25268, "name": "Amber Cytoserocin" },
+  {  "id": 25273, "name": "Golden Cytoserocin" },
+  {  "id": 25274, "name": "Viridian Cytoserocin" },
+  {  "id": 25275, "name": "Celadon Cytoserocin" },
+  {  "id": 25276, "name": "Malachite Cytoserocin" },
+  {  "id": 25277, "name": "Lime Cytoserocin" },
+  {  "id": 25278, "name": "Vermillion Cytoserocin" },
+  {  "id": 25279, "name": "Azure Cytoserocin" },
+  {  "id": 28694, "name": "Amber Mykoserocin" },
+  {  "id": 28695, "name": "Azure Mykoserocin" },
+  {  "id": 28696, "name": "Celadon Mykoserocin" },
+  {  "id": 28697, "name": "Golden Mykoserocin" },
+  {  "id": 28698, "name": "Lime Mykoserocin" },
+  {  "id": 28699, "name": "Malachite Mykoserocin" },
+  {  "id": 28700, "name": "Vermillion Mykoserocin" },
+  {  "id": 28701, "name": "Viridian Mykoserocin" }
+]
+
+fullerite = [
+  {  "id": 30370, "name": "Fullerite-C50" },
+  {  "id": 30371, "name": "Fullerite-C60" },
+  {  "id": 30372, "name": "Fullerite-C70" },
+  {  "id": 30373, "name": "Fullerite-C72" },
+  {  "id": 30374, "name": "Fullerite-C84" },
+  {  "id": 30375, "name": "Fullerite-C28" },
+  {  "id": 30376, "name": "Fullerite-C32" },
+  {  "id": 30377, "name": "Fullerite-C320" },
+  {  "id": 30378, "name": "Fullerite-C540" },
+  {  "id": 49787, "name": "Hiemal Tricarboxyl Vapor" },
+  {  "id": 52193, "name": "Blockout Cube" },
+  {  "id": 52194, "name": "Blockout Cylinder" },
+  {  "id": 52204, "name": "Blockout Sphere" },
+  {  "id": 28629, "name": "Gamboge Cytoserocin" },
+  {  "id": 28630, "name": "Chartreuse Cytoserocin" },
+  {  "id": 50175, "name": "Blockout Cone" }
+]
