404e67177298f68107d88787735fe87eabcf9221
Thomas Walter <speisekatze@gmail.com>
2021-04-30 01:30:57 +0200

    rename stuff, simple cache class

--- src/helper.py
+++ src/helper.py
@@ -1,4 +1,6 @@
-class items:
+import time
+
+class itemlist:
     items = None
     ids = None
     names = None
@@ -14,17 +16,35 @@ class items:
             x[a['name']] = a['id']
         self.names = x.copy()
 
-    def getById(self, id):
+    def get_by_id(self, id):
         return self.ids[id]
 
-    def getByName(self, name):
+    def get_by_name(self, name):
         return self.names[name]
 
-    def getIds(self):
+    def get_ids(self):
         return self.ids.keys()
 
-    def getNames(self):
+    def get_names(self):
         return self.ids.values()
+
+
+class _cache:
+    store = {}
+
+    def set(self, name, region, data):
+        now = time.time()
+        if name not in self.store:
+            self.store[name] = {}
+        self.store[name][region] = { 'ts': now, 'data': data }
+        return self.store[name][region]
+
+    def get(self, name, region):
+        now = time.time()
+        if name in self.store and region in self.store[name] and (now-self.store[name][region]['ts']) < 3600:
+            return self.store[name][region]
+        return None
+
 
 def find_max(market):
     order = {'price': 0.00}
@@ -36,9 +56,9 @@ def find_max(market):
 def find_min(market):
     order = {'price': 0.00}
     for m in market:
-        if float(order['price']) == 0.00:
-            order = m
-            continue
-        if float(m['price']) < float(order['price']):
+        if float(order['price']) == 0.00 or float(m['price']) < float(order['price']):
             order = m
     return order
+
+
+cache = _cache()
