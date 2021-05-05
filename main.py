f9320becfa7c2db07d2494135fa87f431437978d
Thomas Walter <speisekatze@gmail.com>
2021-05-05 04:03:12 +0200

    login info

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from src import market, helper, commands
 from conf.config import bot as botconfig
 
+@commands.bot.event
+async def on_ready():
+    print('Logged in as')
+    print(commands.bot.user.name)
+    print(commands.bot.user.id)
+    print('------')
+
 commands.bot.run(botconfig.token)
